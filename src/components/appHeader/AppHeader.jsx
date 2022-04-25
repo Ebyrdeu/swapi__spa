865298104b26f7663016e68eed3e2c957be38e9a
ebyrd <ebyrdeu.khnykin@gmail.com>
2022-04-25 11:16:40 +0200

    Simple Header with routers Links

---
+++ src/components/appHeader/AppHeader.jsx
@@ -0,0 +1,31 @@
+import {AppBar, Button, Container, Typography} from "@mui/material";
+import {grey} from "@mui/material/colors";
+import {Link} from "react-router-dom";
+
+
+
+// Styled
+const appBarrColor = grey[900];
+const ButtonColor = grey["A100"]
+
+const AppHeader = () => {
+
+	return (
+		<AppBar position='static' style={{background: appBarrColor}}>
+			<Container>
+				<div style={{display:"flex"}}>
+					<Typography variant="h6" component="div" sx={{flexGrow: 1}}>Swapi</Typography>
+
+					<Link to='/' style={{color: ButtonColor,marginLeft: '20px'}}>
+						<Button style={{color: ButtonColor}}>Characters</Button>
+					</Link>
+						<Link to='/person' style={{color: ButtonColor,marginLeft: '20px'}}>
+							<Button style={{color: ButtonColor}}>Movies</Button>
+						</Link>
+				</div>
+			</Container>
+		</AppBar>
+	);
+};
+
+export default AppHeader;
