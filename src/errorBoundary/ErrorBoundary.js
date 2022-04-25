8495b04d05cd8f7b1e4dc6a6e5041db1c2cb7945
ebyrd <ebyrdeu.khnykin@gmail.com>
2022-04-25 11:15:14 +0200

    created Simple Error Boundary

---
+++ src/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
+import {Alert} from "@mui/material";
+
+const Portal = (props) => {
+	const node = document.createElement('div');
+	document.body.appendChild(node);
+	return ReactDOM.createPortal(props.children, node)
+}
+
+
+class ErrorBoundary extends Component {
+	state = {error: false}
+
+	static getDerivedStateFromError() {
+		return { error: true };
+	}
+
+	render() {
+		if (this.state.error === true) return (
+			<Portal>
+				<Alert style={{
+					position: 'absolute',
+					bottom: '10px',
+					left: '10px',
+					zIndex: '999999',
+				}} severity="error">Something Went Wrong - Reset page</Alert>
+			</Portal>
+		)
+		return this.props.childElement
+	}
+}
+
+export default ErrorBoundary;
