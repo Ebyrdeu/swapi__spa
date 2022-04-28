f808905811b926a74b9c9b7e256cb28aaaee2623
ebyrd <ebyrdeu.khnykin@gmail.com>
2022-04-28 17:38:43 +0200

    1. Deleted old obj state 2. Added new Route for single movie or character 3. Added new Route for movies List

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,33 +2,21 @@ import ErrorBoundary from "../../errorBoundary/ErrorBoundary";
 import AppHeader from "../appHeader/AppHeader";
 import AppCharacters from "../appCharacters/AppCharacters";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {useState} from "react";
-import AppMoreInfo from "../appMoreInfo/appMoreInfo";
+import AppMovies from "../appMovies/AppMovies";
+import AppInfo from "../appInfo/AppInfo";
+
 
 
 function App() {
-	const [learnMore, setLearnMore] = useState({
-		name: null,
-		height: null,
-		mass: null,
-		hair_color: null,
-		skin_color: null,
-		eye_color: null,
-		birth_year: null,
-		gender: null,
-		homeworld: null,
-		films: null
-	});
-
-	const onLearMore = (data) => setLearnMore({...data})
 
 	return (
 		<Router>
 			<div className="App">
 				<AppHeader/>
 				<Routes>
-					<Route exact path="/" element={<ErrorBoundary childElement={<AppCharacters onLearMore={onLearMore}/>}/>}/>
-					<Route exact path='/person' element={<ErrorBoundary childElement={<AppMoreInfo learnMore={learnMore}/>}/>}/>
+					<Route path="/" element={<ErrorBoundary childElement={<AppCharacters/>}/>}/>
+					<Route path="/movies" element={<ErrorBoundary childElement={<AppMovies/>}/>}/>
+					<Route path="/info/:type/:id" element={<ErrorBoundary childElement={<AppInfo/>}/>}/>
 				</Routes>
 			</div>
 		</Router>
