2c5e7300ae18772913e363cc649ba28cfdf11531
ebyrd <ebyrdeu.khnykin@gmail.com>
2022-04-28 17:32:29 +0200

    1 Change routers routing /person > /movies

--- src/components/appHeader/AppHeader.jsx
+++ src/components/appHeader/AppHeader.jsx
@@ -3,7 +3,6 @@ import {grey} from "@mui/material/colors";
 import {Link} from "react-router-dom";
 
 
-
 // Styled
 const appBarrColor = grey[900];
 const ButtonColor = grey["A100"]
@@ -13,15 +12,14 @@ const AppHeader = () => {
 	return (
 		<AppBar position='static' style={{background: appBarrColor}}>
 			<Container>
-				<div style={{display:"flex"}}>
+				<div style={{display: "flex"}}>
 					<Typography variant="h6" component="div" sx={{flexGrow: 1}}>Swapi</Typography>
-
-					<Link to='/' style={{color: ButtonColor,marginLeft: '20px'}}>
+					<Link to='/' style={{color: ButtonColor, marginLeft: '20px'}}>
 						<Button style={{color: ButtonColor}}>Characters</Button>
 					</Link>
-						<Link to='/person' style={{color: ButtonColor,marginLeft: '20px'}}>
-							<Button style={{color: ButtonColor}}>Movies</Button>
-						</Link>
+					<Link to='/movies' style={{color: ButtonColor, marginLeft: '20px'}}>
+						<Button style={{color: ButtonColor}}>Movies</Button>
+					</Link>
 				</div>
 			</Container>
 		</AppBar>
