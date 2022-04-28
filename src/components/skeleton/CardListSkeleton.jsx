d6c7948aecad871784f83f0021240ea5f00f6353
ebyrd <ebyrdeu.khnykin@gmail.com>
2022-04-28 17:25:51 +0200

    Adapted width to card size

--- src/components/skeleton/CardListSkeleton.jsx
+++ src/components/skeleton/CardListSkeleton.jsx
@@ -2,17 +2,21 @@ import {Skeleton} from "@mui/material";
 
 const CardListSkeleton = () => {
 	return (
-		<div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
-			<Skeleton style={{margin: 8}} variant="rectangular" width={200} height={170} />
+		<div style={{display: "inline"}}>
+
+
+		<div style={{display: "flex", flexWrap: "wrap"}}>
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+			<Skeleton style={{margin: 8}} variant="rectangular" width={250} height={170} />
+		</div>
 		</div>
 	);
 };
